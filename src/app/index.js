49e76d749ee80de725a50c320c1445291b711f08
gowthami666 <gowthami.mobile@gmail.com>
2017-10-28 11:13:27 -0500

    Multiple components

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import {render} from "react-dom";
-
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
+ 
 class App extends React.Component{
     render(){
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-xs-10">
-                        <h1>hello</h1>
-                    </div>
+                <Header/>
+                </div>
+                <div className="row">
+                <Home/>
                 </div>
             </div>
         );
